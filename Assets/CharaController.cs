95592739d282f5ffeceaa5de3dc0178c360ca847
ruki hamaguchi <king-king-7@ezweb.ne.jp>
2017-11-04 16:58:27 +0900

    ghost00
    ghost00

---
+++ Assets/CharaController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharaController : MonoBehaviour {
+
+	private Rigidbody2D rigid2D;
+
+	private float turnForce = 400.0f;
+
+	// Use this for initialization
+	void Start () {
+
+		this.rigid2D = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+
+			GetComponent<Animator> ().SetTrigger ("jumpTrigger");
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+
+			rigid2D.AddForce (transform.up * 400);
+		}
+
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+
+			rigid2D.AddForce (new Vector2(-200, 0));
+		
+		} else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+
+			rigid2D.AddForce (new Vector2(200, 0));
+				}
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+
+		if (col.gameObject.tag == "Bullet") {
+
+			SceneManager.LoadScene ("GameoverScene");
+		}
+	}
+}
---
+++ Assets/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public GameObject bullet;
+
+	IEnumerator Start() {
+
+		while (true) {
+
+			Instantiate (bullet, transform.position, transform.rotation);
+
+			yield return new WaitForSeconds (2.0f);
+		}
+	}
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
